3fafb457df291bb355cd24a4c03ab484021be9b7
bt.will777@gmail.com <bwilli387@HQSWL-C040684.cable.comcast.com>
2020-02-19 20:48:29 -0500

    cart add/remove update

--- B_Ecommerce-master/core/views.py
+++ B_Ecommerce-master/core/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import redirect
@@ -33,13 +34,18 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
+            messages.info(request, "This item quantity was updated")
+            return redirect("core:product-page", slug=slug)
         else:
             order.items.add(order_item)
+            messages.info(request, "This item was added to your cart")
+            return redirect("core:product-page", slug=slug)
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
-    return redirect("core:product-page", slug=slug)
+        messages.info(request, "This item was added to your cart")
+        return redirect("core:product-page", slug=slug)
     
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
@@ -57,10 +63,13 @@ def remove_from_cart(request, slug):
             )[0]
             order.items.remove(order_item)
             order_item.delete()
+            messages.info(request, "This item was removed from your cart")
+            return redirect("core:product-page", slug=slug)
         else:
             # add a message saying the user doesn't have an order
+            messages.info(request, "This item was not in your cart")
             return redirect("core:product-page", slug=slug)
     else:
         # add a message saying the user doesnt have an order
+        messages.info(request, "You do not have an active order")
         return redirect("core:product-page", slug=slug)
-    return redirect("core:product-page", slug=slug)
