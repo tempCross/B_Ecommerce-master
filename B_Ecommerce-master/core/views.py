d27a0710b80ad10775a543a4896628c50cd84610
bt.will777@gmail.com <bwilli387@HQSWL-C040684.cable.comcast.com>
2020-02-23 12:09:28 -0500

    add/remove from cart

--- B_Ecommerce-master/core/views.py
+++ B_Ecommerce-master/core/views.py
@@ -54,17 +54,17 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "This item quantity was updated")
-            return redirect("core:product-page", slug=slug)
+            return redirect("core:order-summary")
         else:
             order.items.add(order_item)
             messages.info(request, "This item was added to your cart")
-            return redirect("core:product-page", slug=slug)
+            return redirect("core:order-summary")
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "This item was added to your cart")
-        return redirect("core:product-page", slug=slug)
+        return redirect("core:order-summary")
     
 @login_required    
 def remove_from_cart(request, slug):
@@ -93,3 +93,32 @@ def remove_from_cart(request, slug):
         # add a message saying the user doesnt have an order
         messages.info(request, "You do not have an active order")
         return redirect("core:product-page", slug=slug)
+
+@login_required    
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user, ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order_item.quantity -= 1
+            order_item.save()
+            messages.info(request, "This item quantity was updated")
+            return redirect("core:order-summary")
+        else:
+            # add a message saying the user doesn't have an order
+            messages.info(request, "This item was not in your cart")
+            return redirect("core:product-page", slug=slug)
+    else:
+        # add a message saying the user doesnt have an order
+        messages.info(request, "You do not have an active order")
+        return redirect("core:product-page", slug=slug)
+
