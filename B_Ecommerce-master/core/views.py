0b8ced97420e1d9f1140e2363d1775f1a1331ead
bt.will777@gmail.com <bwilli387@HQSWL-C040684.cable.comcast.com>
2020-02-26 13:25:26 -0500

    stripe data updated 2

--- B_Ecommerce-master/core/views.py
+++ B_Ecommerce-master/core/views.py
@@ -35,7 +35,11 @@ class OrderSummaryView(LoginRequiredMixin, View):
 class PaymentView(View):
     def get(self, *args, **kwargs):
         #order
-        return render(self.request, "payment.html")   
+        order = Order.objects.get(user=self.request.user, ordered=False) 
+        context = {
+            'order': order
+        }
+        return render(self.request, "payment.html", context)   
    
     def post(self, *args, **kwargs):
         order = Order.objects.get(user=self.request.user, ordered=False)
