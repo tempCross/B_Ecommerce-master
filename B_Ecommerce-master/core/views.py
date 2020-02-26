8c80ef7a4e2b219e436e72553266044f9d357d79
bt.will777@gmail.com <bwilli387@HQSWL-C040684.cable.comcast.com>
2020-02-26 12:48:37 -0500

    stripe data updated

--- B_Ecommerce-master/core/views.py
+++ B_Ecommerce-master/core/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
@@ -5,10 +6,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, View
 from django.shortcuts import redirect
-from .models import Item, OrderItem, Order, BillingAddress
+from .models import Item, OrderItem, Order, BillingAddress, Payment
 from django.utils import timezone
 from .forms import CheckoutForm
-# Create your views here.
+
+import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class HomeView(ListView):
     model = Item
@@ -31,7 +34,78 @@ class OrderSummaryView(LoginRequiredMixin, View):
 
 class PaymentView(View):
     def get(self, *args, **kwargs):
+        #order
         return render(self.request, "payment.html")   
+   
+    def post(self, *args, **kwargs):
+        order = Order.objects.get(user=self.request.user, ordered=False)
+        token = self.request.POST.get('stripeToken')
+        amount=int(order.get_total() * 100) # cents
+        try:
+            # Use Stripe's library to make requests...
+            charge = stripe.Charge.create(
+            amount=amount, # cents
+            currency="usd",
+            source=token
+        )
+
+            # create payment
+            payment = Payment()
+            payment.stripe_charge_id = charge['id']
+            payment.user = self.request.user
+            payment.amount = order.get_total()
+            payment.save()
+
+            # assign the payment to the order
+            order.ordered = True
+            order.payment = payment
+            order.save()
+            
+            messages.success(self.request, "Your order was successful")
+            return redirect("/")
+
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+            body = e.json_body
+            err = body.get('error', {})
+            messages.error(self.request, f"{err.get('message')}")
+            return redirect("/")
+
+        except stripe.error.RateLimitError as e:
+        # Too many requests made to the API too quickly
+            messages.error(self.request, "Rate limit error")
+            return redirect("/")
+
+        except stripe.error.InvalidRequestError as e:
+        # Invalid parameters were supplied to Stripe's API
+            messages.error(self.request, "Invalid parameters")
+            return redirect("/")
+
+        except stripe.error.AuthenticationError as e:
+        # Authentication with Stripe's API failed
+        # (maybe you changed API keys recently)
+            messages.error(self.request, "Not authenticated")
+            return redirect("/")
+
+        except stripe.error.APIConnectionError as e:
+        # Network communication with Stripe failed
+            messages.error(self.request, "Network error")
+            return redirect("/")
+
+        except stripe.error.StripeError as e:
+        # Display a very generic error to the user, and maybe send
+        # yourself an email
+            messages.error(self.request, "Something went wrong you were not charged. Please try again")
+            return redirect("/")
+
+        except Exception as e:
+        # send an email to ourselves
+            messages.error(self.request, "A serious error occured. We have been notified")
+            return redirect("/")
+        
+
+    
+
 class CheckoutView(View):
     def get(self, *args, **kwargs):
         # form
@@ -47,27 +121,33 @@ class CheckoutView(View):
             order = Order.objects.get(user=self.request.user, ordered=False)
 
             if form.is_valid():
-                street_address = form.cleaned_data('street_address')
-                country = form.cleaned_data('country')
-                zip = form.cleaned_data('zip')
+                street_address = form.cleaned_data.get('street_address')
+                country = form.cleaned_data.get('country')
+                zip = form.cleaned_data.get('zip')
                 #TODO: add functionality for these fields
                 # same_billing_address = form.cleaned_data('same_shipping_address')
                 # save_info = form.cleaned_data('save_info')
-                payment_option = form.cleaned_data('payment_option')
+                payment_option = form.cleaned_data.get('payment_option')
                 billing_address = BillingAddress(
                     user=self.request.user,
                     street_address=street_address,
-                    apartment_address=apartment_address,
+                    apartment_address=street_address+country+zip,
                     country=country,
                     zip=zip
                 )
                 billing_address.save()
                 order.billing_address = billing_address
                 order.save()
+
+                if payment_option == 'S':
+                    return redirect('core:payment', payment_option='stripe')
+                elif payment_option == 'P':
+                    return redirect('core:payment', payment_option='paypal')
                 #TODO add redirect to the selected payment option
-                return redirect('core:checkout')
-            messages.warning(self.request,"Failed checkout")
-            return redirect('core:checkout')
+                else:
+                    messages.warning(self.request,"Invalid payment option")
+                    return redirect('core:checkout')   
+            
         except ObjectDoesNotExist:
             messages.error(self.request, "You don't have an active order")
             return redirect("core:order-summary")
